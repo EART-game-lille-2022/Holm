35b22a39fef4eaa23b96baa20fa1aa1bb132818c
Arthur Caumartin <artcaumart@gmail.com>
2024-02-03 20:28:12 +0100

    comment and rename on WindPoint

--- Assets/[Project]/Scripts/NelloStuff/WindPoint.cs
+++ Assets/[Project]/Scripts/NelloStuff/WindPoint.cs
@@ -5,39 +5,44 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class WindPoint : MonoBehaviour
 {
-    public static List<WindPoint> points = new List<WindPoint>();
+    public static List<WindPoint> pointsList = new List<WindPoint>();
     public float value = 1;
     // Start is called before the first frame update
     void OnEnable()
     {
-        points.Add(this);
+        pointsList.Add(this);
     }
 
     // Update is called once per frame
     void OnDisable()
     {
-        points.Remove(this);
+        pointsList.Remove(this);
     }
 
     public static void GetWeightAt(Vector3 position, out float value, out Vector3 scale, out Vector3 forward)
     {
         float currentWeight = 0;
-        value= 0;
-        scale= Vector3.zero;
+        value = 0;
+        scale = Vector3.zero;
         forward = Vector3.zero;
-        foreach (var point in points)
+        foreach (var point in pointsList)
         {
-            float d = Mathf.Max(0.0001f,(position - point.transform.position).magnitude);
-            //d = d * d;
-            float weight = 1f / d;
-            scale += point.transform.localScale * weight;
+            //! Max pour eviter la division par 0
+            float distance = Mathf.Max(0.0001f, (position - point.transform.position).magnitude);
+            // d = d * d;
+            //! design patern pour get l'influence de plusieur points?
+            float weight = 1f / distance;
+
             value += point.value * weight;
+            scale += point.transform.localScale * weight;
             forward += point.transform.forward * weight;
+
             currentWeight += weight;
         }
-        scale /= currentWeight; 
+
+        scale /= currentWeight;
         value /= currentWeight;
-        forward/= currentWeight;
+        forward /= currentWeight;
 
         //Vector2.Angle(forward, Vector.up);
         //float a = Mathf.Atan2(forward.x, -forward.y) * Mathf.Rad2Deg;
