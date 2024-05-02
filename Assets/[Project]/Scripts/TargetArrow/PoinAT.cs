2fc7bf837d845ef5413de11371e8d746f0045b58
Pix-Hell <145968748+PixHell02@users.noreply.github.com>
2024-05-02 11:20:52 +0200

    "arrow aled"

--- Assets/[Project]/Scripts/TargetArrow/PoinAT.cs
+++ Assets/[Project]/Scripts/TargetArrow/PoinAT.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class PoinAT : MonoBehaviour
 {
-    public Transform targetObject; // L'objet que la flèche doit pointer
+    public List<Collectible> targetList; // L'objet que la flèche doit pointer
     public float rotationSpeed = 5f; // Vitesse de rotation de la flèche
-
-    void Update()
+    void Start()
     {
-        // Vérifie si l'objet cible est défini
-        if (targetObject != null)
-        {
-            // Calcul de la direction vers l'objet cible
-            Vector3 directionToTarget = targetObject.position - transform.position;
+        QuestManager.instance.OnQuestStart.AddListener(getTarget);
 
-            // Calcul de la rotation nécessaire pour faire pointer la flèche vers l'objet cible
-            Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
-
-            // Rotation progressive de la flèche vers la rotation cible
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
-        else
-        {
-            Debug.LogWarning("L'objet cible n'est pas défini !");
-        }
     }
+    public void getTarget(List<Collectible> targetList, QuestTarget questTarget)
+    {
+        this.targetList = targetList;
+        print("YOOOOOO");
+    }
+
+    // void Update()
+    // {
+    //     // Vérifie si l'objet cible est défini
+    //     if (targetList != null)
+    //     {
+    //         // Calcul de la direction vers l'objet cible
+    //         Vector3 directionToTarget = targetList.position - transform.position;
+
+    //         // Calcul de la rotation nécessaire pour faire pointer la flèche vers l'objet cible
+    //         Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+
+    //         // Rotation progressive de la flèche vers la rotation cible
+    //         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    //     }
+    //     else
+    //     {
+    //         Debug.LogWarning("L'objet cible n'est pas défini !");
+    //     }
+    // }
 }
